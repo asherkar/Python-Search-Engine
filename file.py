b5139d5e1f5de6c03ba6b4fe683ed092df201fa0
gurkiratsingh <gurkirat1.singh@ryerson.ca>
2019-09-28 18:40:26 -0400

    added loop to go through the file

--- file.py
+++ file.py
@@ -1,4 +1,14 @@
-f=open("cacm.all","r")
+#!/usr/bin/env python
+import re
 
-for i in range(30):
-    print(f.read())
+documents = {}
+f = open('cacm/cacm.all', 'r')
+
+for line in f:
+    if '.I ' in line:
+        id = re.sub('.I ', '', line)
+        documents[id] = {'id' : id}
+        print(id)
+    if '.T' in line:
+        print(f.next())
+
