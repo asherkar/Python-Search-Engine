be1733c4c6d19f6555dcda4176d5ffc64ad72076
gurkiratsingh <gurkirat1.singh@ryerson.ca>
2019-09-28 23:40:42 -0400

    more fixes

--- file.py
+++ file.py
@@ -4,27 +4,33 @@ import re
 documents = {}
 f = open('cacm/test.txt', 'r')
 
-for line in f:
-    line = line.rstrip()
-    print(line)
-    if '.I ' in line:
-        id = re.sub('.I ', '', line)
-        print(id)
-        documents[id] = {}
-        documents[id]['id'] = id
-        nextLine = f.next()
+line = f.readline().rstrip()
+while line:
+    nextLine = None
 
-        while(not ('.I ' in nextLine)):
+    if '.I ' in line:
+        docId = re.sub('.I ', '', line)
+        documents[docId] = {}
+        documents[docId]['id'] = docId
+        nextLine = f.readline().rstrip()
+        print(nextLine)
+
+        while nextLine and not ('.I ' in nextLine):
+            print(nextLine)
             if '.T' in nextLine:
-                documents[id]['title'] = f.next()
+                documents[docId]['title'] = f.readline()
             if '.W' in nextLine:
-                nextLine = f.next()
+                nextLine = f.readline().rstrip()
                 abstract = ''
-                while(re.match(r'^[.][A-Z]\s', nextLine)):                    
-                    abstract+= nextLine
-                    nextLine = f.next()
-                documents[id]['abstract'] = abstract
-            nextLine = f.next()
+                while nextLine and not re.match(r'^[.][A-Z]\s', nextLine):
+                    print(nextLine)
+                    abstract += ' ' + nextLine
+                    nextLine = f.readline().rstrip()
+                documents[docId]['abstract'] = abstract
+            nextLine = f.readline().rstrip()
+            print(nextLine)
+
+    line = f.readline() if nextLine is None else nextLine
         
     
 print(documents)
