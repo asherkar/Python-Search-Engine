3bfd366fae17ffd22b3811d97255b723353a4ff8
gurkiratsingh <gurkirat1.singh@ryerson.ca>
2019-09-28 22:43:37 -0400

    working something

--- file.py
+++ file.py
@@ -2,13 +2,30 @@
 import re
 
 documents = {}
-f = open('cacm/cacm.all', 'r')
+f = open('cacm/test.txt', 'r')
 
 for line in f:
+    line = line.rstrip()
+    print(line)
     if '.I ' in line:
         id = re.sub('.I ', '', line)
-        documents[id] = {'id' : id}
         print(id)
-    if '.T' in line:
-        print(f.next())
+        documents[id] = {}
+        documents[id]['id'] = id
+        nextLine = f.next()
+
+        while(not ('.I ' in nextLine)):
+            if '.T' in nextLine:
+                documents[id]['title'] = f.next()
+            if '.W' in nextLine:
+                nextLine = f.next()
+                abstract = ''
+                while(re.match(r'^[.][A-Z]\s', nextLine)):                    
+                    abstract+= nextLine
+                    nextLine = f.next()
+                documents[id]['abstract'] = abstract
+            nextLine = f.next()
+        
+    
+print(documents)
 
