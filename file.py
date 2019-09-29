da839ee715b31c676a07181efe8fa7bc01b54c3c
gurkiratsingh <gurkirat1.singh@ryerson.ca>
2019-09-29 00:07:27 -0400

    fixed issue with fetching abstract

--- file.py
+++ file.py
@@ -4,30 +4,31 @@ import re
 documents = {}
 f = open('cacm/test.txt', 'r')
 
-line = f.readline().rstrip()
+line = f.readline()
 while line:
+    line = line
     nextLine = None
 
     if '.I ' in line:
-        docId = re.sub('.I ', '', line)
+        docId = re.sub('.I ', '', line).rstrip()
         documents[docId] = {}
         documents[docId]['id'] = docId
-        nextLine = f.readline().rstrip()
+        nextLine = f.readline()
         print(nextLine)
 
         while nextLine and not ('.I ' in nextLine):
-            print(nextLine)
             if '.T' in nextLine:
-                documents[docId]['title'] = f.readline()
+                documents[docId]['title'] = f.readline().rstrip()
             if '.W' in nextLine:
-                nextLine = f.readline().rstrip()
+                nextLine = f.readline()
                 abstract = ''
-                while nextLine and not re.match(r'^[.][A-Z]\s', nextLine):
+                while nextLine and not re.match(r'[.][A-Z]\s', nextLine):
+                    print(re.match(r'[.][A-Z]\s', nextLine))
                     print(nextLine)
-                    abstract += ' ' + nextLine
-                    nextLine = f.readline().rstrip()
+                    abstract += ' ' + nextLine.rstrip()
+                    nextLine = f.readline()
                 documents[docId]['abstract'] = abstract
-            nextLine = f.readline().rstrip()
+            nextLine = f.readline()
             print(nextLine)
 
     line = f.readline() if nextLine is None else nextLine
